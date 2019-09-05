a6eff3846f547093d934d28fc89f908192f8af3d
ShisiJu <jushisi01@163.com>
2019-09-05 10:51:27 +0800

    新增:添加日期处理工具类

---
+++ code-generator/src/main/java/tool/util/DateUtils.java
@@ -0,0 +1,20 @@
+package tool.util;
+
+import java.util.Calendar;
+
+/**
+ * @author Administrator
+ * @date 2019/9/5
+ */
+
+public class DateUtils {
+
+    public static String getDate() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int date = calendar.get(Calendar.DATE);
+        return year + "-" + month + "-" + date;
+    }
+
+}
