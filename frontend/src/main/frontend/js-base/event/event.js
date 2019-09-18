b6a32c21034047e1e666ae8fd3bcbb4468727579
ShisiJu <jushisi01@163.com>
2019-09-18 10:15:52 +0800

    修改:将on,off放入实例方法,处理成class

--- frontend/src/main/frontend/js-base/event/event.js
+++ frontend/src/main/frontend/js-base/event/event.js
@@ -1,28 +1,81 @@
 let div = document.querySelector("#div");
 
-let eventMap = {};
 
-let on = function (dom, type, callback) {
-    eventMap[type] = new Event(type);
-    dom.addEventListener(type, callback, false);
+class MyJquery {
+    constructor(dom) {
+        var ele = {};
+        if (dom instanceof HTMLDivElement) {
+            ele = dom;
+        } else if (typeof dom === "string") {
+            ele = document.querySelector(dom);
+        }
+        this.ele = ele;
+        this.events = {};
+    }
 
-};
+    toString() {
+        return '(' + this.ele + ')';
+    }
 
-let fire = function (dom, type) {
-    dom.dispatchEvent(eventMap[type]);
-};
+    on(type, data, callback) {
+        // 没有传data,只有两个参数
+        if (typeof data === "function") {
+            callback = data;
+            data = {};
+
+        }
+
+        if (typeof type === "string") {
+            if (!this.events [type]) {
+                this.events [type] = [];
+            }
+            // 元素上可能存在多个同名事件
+            this.events [type].push(new CustomEvent(type, {detail: data}));
+        }
+
+        this.ele.addEventListener(type, callback);
+    }
+
+    fire(type) {
+        if (!this.events [type]) {
+            this.events [type] = [];
+        }
+
+        if (this.events[type].length === 0) {
+            return;
+        }
+
+        this.events[type].forEach(event => {
+            this.ele.dispatchEvent(event);
+        })
+
+    }
+}
 
 
-on(div, "build", function (e) {
-    console.log(e);
-    console.log("build ---- fire --- ");
+function print(str) {
+    console.log(str)
+
+}
+
+
+let $div = new MyJquery("#div");
+
+print($div);
+
+
+$div.on("build", {}, function (e) {
+    print(" build ---- on");
 });
 
 
-div.addEventListener("click", function (evt) {
-    console.log(" click ");
-    fire(div, "build");
-});
+$div.fire("build");
+
+
+
+
+
+
 
 
 
